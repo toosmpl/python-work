c03cae4c7f92d933feb0644558469a0b00f9467e
toosmpl <sergeyaleko@mail.ru>
2024-08-20 20:58:17 +0300

    Add files via upload

--- module3.4.py
+++ module3.4.py
@@ -1,7 +1,7 @@
 def single_root_words(root_word, *other_words):
     same_words = []
     for i in other_words:
-        if root_word.lower().count(i.lower()) or i.count(root_word.lower()):
+        if root_word.lower().count(i.lower()) or i.lower().count(root_word.lower()):
             same_words.append(i)
     return same_words
 
