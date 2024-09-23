b33fd57120023de1ae87cfc580ba2ac6a9e4c29e
toosmpl <sergeyaleko@mail.ru>
2024-09-23 15:45:52 +0300

    Add files via upload

---
+++ module7.3.py
@@ -0,0 +1,40 @@
+import io
+from pprint import pprint
+
+class WordsFinder:
+
+    def __init__(self, file_names):
+        self.file_names = file_names
+
+    def get_all_words(self):
+        all_words = {}
+        j = ''
+        punc = [',', '.', '=', '!', '?', ';', ':', ' - ']
+        with open(self.file_names, encoding='utf-8') as file:
+            for line in file:
+                line = line.lower()
+                for i in line:
+                   if i in punc:
+                        line = line.replace(i,'')
+                j = j + line
+            all_words.update({self.file_names:j.split()})
+        return all_words
+
+    def find(self,txt):
+        dist = {}
+        word = self.get_all_words()[self.file_names]
+        for i in range(len(word)):
+            if txt.lower() == word[i]:
+                dist.update({self.file_names: i+1})
+                return dist
+
+    def count(self, txt):
+        dist = {}
+        word = self.get_all_words()[self.file_names]
+        dist.update({self.file_names: word.count(txt.lower())})
+        return dist
+
+finder2 = WordsFinder('test_file.txt')
+print(finder2.get_all_words())
+print(finder2.find('TEXT'))
+print(finder2.count('teXT'))
